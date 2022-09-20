ad3b1a5800c0513b5d344c054caffe6f52be6f68
AlfredoRD <61297115+AlfredoRD@users.noreply.github.com>
2022-09-20 11:48:12 -0400

    guardado

---
+++ src/components/Table.vue
@@ -0,0 +1,46 @@
+<template>
+  
+  <h1>couter</h1>
+  <p>{{counter}} <sup>2</sup> = {{squereq}} </p>
+  <p>{{counter}} <sup>2</sup> = {{squere()}} </p>
+
+  <p>{{counter}} <sup>2</sup> = {{squereq}} </p>
+  <p>{{counter}} <sup>2</sup> = {{squere()}} </p>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'MyTable',
+
+
+  data(){
+    return {counter: 5}
+  },
+
+  methods:{
+  squere(){
+ console.log('klk')
+    return this.counter * this.counter
+  }
+    
+},
+
+computed:{
+
+  squereq() {
+     console.log('kkaka')
+    return this.counter * this.counter
+  }  
+}
+
+}
+
+
+</script>
+
+<style>
+
+</style>
