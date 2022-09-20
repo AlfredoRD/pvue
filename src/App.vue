7842be528d308c0c6a0deca6d2c171d25acc4162
AlfredoRD <61297115+AlfredoRD@users.noreply.github.com>
2022-09-20 14:59:09 -0400

    actualizado

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <MyTable/>
+  <MyTable :counters="100" />
+ 
+  
 </template>
 
 <script>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,11 +1,16 @@
+import { title } from 'process';
 <template>
   
-  <h1>couter</h1>
-  <p>{{counter}} <sup>2</sup> = {{squereq}} </p>
+  <h1>{{changes }}</h1>
   <p>{{counter}} <sup>2</sup> = {{squere()}} </p>
-
-  <p>{{counter}} <sup>2</sup> = {{squereq}} </p>
-  <p>{{counter}} <sup>2</sup> = {{squere()}} </p>
+  <p>{{name}}</p>
+  
+  <div>
+    <button class="bto" v-on:click="incremetar">+1</button>
+    <button class="bto" v-on:click="descre">-1</button>
+    <button class="bto" v-on:click="change">name</button> 
+  </div>
+  
 
 </template>
 
@@ -13,17 +18,42 @@
 
 
 export default {
-  name: 'MyTable',
 
+
+  name: 'MyTable',
+  props: {
+    title: String,
+    counters: {
+      type: Number,
+      default: 100,
+      //required: true,
+      validator(value){
+        return value > 100
+      }
+
+    }
+
+  },
 
   data(){
-    return {counter: 5}
+    return {counter: this.counters, name: 'alfredo'}
+    
   },
 
   methods:{
   squere(){
- console.log('klk')
     return this.counter * this.counter
+  },
+
+  incremetar(){
+    return this.counter++
+  },
+  descre(){
+    return this.counter--
+  },
+
+  change(){
+    this.name = 'Elias'
   }
     
 },
@@ -33,14 +63,32 @@ computed:{
   squereq() {
      console.log('kkaka')
     return this.counter * this.counter
-  }  
+  } ,
+  
+  changes(){
+       
+    return this.title || this.name
+      
+  }
 }
 
 }
-
 
 </script>
 
 <style>
+ 
+ .bto{
+
+  background-color: aquamarine;
+  border-radius: 1px solido #fff;
+  margin: 0 5px;
+  padding: 10px;
+
+ }
+
+ .bto:hover{
+  background-color: antiquewhite;
+ }
 
 </style>
